8013bf74a74c64e28ca75572e9cfb0d6909f1565
srik212 <srik120299@gmail.com>
2026-07-09 12:26:00 -0400

    Changed port number in backend folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,5 +17,5 @@ app.use("/api/posts", commentRoutes); // mounts /:postId/comments under /api/pos
 
 app.use((_req, res) => res.status(404).json({ error: "Not found" }));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 API listening on http://localhost:${PORT}`));
